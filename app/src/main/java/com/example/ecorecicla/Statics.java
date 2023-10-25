9d475ec2f5e206324674dac78d9f77b55f8a9e1f
DJOF81 <juorti@hotmail.es>
2023-10-25 18:57:17 -0500

    Update

--- app/src/main/java/com/example/ecorecicla/Statics.java
+++ app/src/main/java/com/example/ecorecicla/Statics.java
@@ -3,21 +3,12 @@ package com.example.ecorecicla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Statics extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
-
+        setContentView(R.layout.activity_statics);
     }
-
-
-
 }

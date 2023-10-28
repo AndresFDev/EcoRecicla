a75b8c1b92d12b0c3a189e1a3c8c219d3439acf6
Your Name <arocasandra8@gmail.com>
2023-10-27 21:57:57 -0500

    Upload

--- app/src/main/java/com/example/ecorecicla/MainActivity.java
+++ app/src/main/java/com/example/ecorecicla/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
     Button loginbotton;
     @Override

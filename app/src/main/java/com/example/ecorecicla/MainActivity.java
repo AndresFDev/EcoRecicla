a99ac8f7a182dba409563762682348f4f2b80a27
Luz <luzrodriguez21@outlook.com>
2023-10-28 18:06:53 -0500

    upload

--- app/src/main/java/com/example/ecorecicla/MainActivity.java
+++ app/src/main/java/com/example/ecorecicla/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button tipsplastico;
+    Button tipspapel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tipsplastico = findViewById(R.id.button4);
+        tipspapel = findViewById(R.id.button5);
+
+        Intent tipsplastico2= new Intent(getApplicationContext(), TipsplasticActivity2.class);
+        Intent tipspapel2= new Intent(getApplicationContext(), TipsPapelActivity2.class);
+
+        tipsplastico .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(tipsplastico2);
+            }
+        });
+
+        tipspapel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(tipspapel2);}
+        });
+
     }
 }
